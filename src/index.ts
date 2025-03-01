2e4d305fb0cd4e57fc940b2e2ae10cfd6a33448e
electroluxcode <electroluxcoder@gmail.com>
2025-03-01 14:39:41 +0800

    chore: 去除注释

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 ﻿import * as t from '@babel/types';
 import node_path from 'path';
 
-// Define plugin options interface
 interface PluginOptions {
   enableInProduction?: boolean;
 }
 
-// type 
 export default function babelPluginAntdStyle(api: any, options: PluginOptions = {}) {
   const { enableInProduction = false } = options;
   if (process.env.NODE_ENV === 'production' && !enableInProduction) {
